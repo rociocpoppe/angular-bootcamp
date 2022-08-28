aeffe05dc3f9d88ac22e1afdbfeef678150a57fd
Rocio Poppe <rociocpoppe@gmail.com>
2022-08-27 23:36:20 -0300

    se agregan componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
+import { IndivualTaskComponent } from './components/indivual-task/indivual-task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTaskComponent,
+    IndivualTaskComponent,
+    TaskListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
